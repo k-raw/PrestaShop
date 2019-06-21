f436398e946f7226788329b2dc37688a10132a16
k-raw <quentin@shokola.com>
2019-06-21 14:58:47 +0200

    Prevent importing product with no name

--- controllers/admin/AdminImportController.php
+++ controllers/admin/AdminImportController.php
@@ -232,7 +232,6 @@ class AdminImportControllerCore extends AdminController
                     'AdminImportController',
                     'split',
                 );
-
                 
                 $match_ref = Tools::getValue('match_ref');
                 $force_ids = Tools::getValue('forceIDs');
